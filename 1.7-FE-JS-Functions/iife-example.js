984ab0e88cbdbd71b6407471e8ebf87f8ef1adab
CHINNAKOTLAJAGANNATH <101012974+CHINNAKOTLAJAGANNATH@users.noreply.github.com>
2024-12-27 14:58:40 +0530

    Add files via upload

---
+++ 1.7-FE-JS-Functions/iife-example.js
@@ -0,0 +1,10 @@
+// Example 1:
+(function () {
+    console.log('IIFE');
+    })();
+    
+    // Example 2:
+    (function () {
+    var greet = 'Hello';
+    console.log(greet);
+    })();
---
+++ 1.7-FE-JS-Functions/namedfunc.js
@@ -0,0 +1,9 @@
+//create a function to call area of rectangle 
+// and return the result
+
+function area(l,b)
+{
+    return l*b;
+}
+console.log(area(4,5));
+
