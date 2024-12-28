45f62ab2d2461b127ed3685dd6a9d5e1fe37cb0c
CHINNAKOTLAJAGANNATH <101012974+CHINNAKOTLAJAGANNATH@users.noreply.github.com>
2024-12-28 15:49:22 +0530

    Create dbconfig.go

---
+++ Go-Lang-2-Assignment/go-sqlite-crud-project-assignment/config/dbconfig.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+
+	_ "modernc.org/sqlite" // SQLite driver
+)
+
+var DB *sql.DB
+
+func InitializeDatabase() {
+	var err error
+	DB, err = sql.Open("sqlite", "./myproducts.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create Products table if not exists
+	_, err = DB.Exec(`CREATE TABLE IF NOT EXISTS products (
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		name TEXT,
+		price INTEGER,
+		qty INTEGER
+	);`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetDB() *sql.DB {
+	return DB
+}
